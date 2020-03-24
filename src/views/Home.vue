50b98e361efa41a970a7354dee2c7352f1059bdc
pdeng <635639654@qq.com>
2020-03-24 23:12:07 +0800

    ..

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,14 @@
     <h3>我是首页</h3>
     <div v-for="i in 10" :key="i" :id="`line-chart-${i}`" style="height: 400px"></div>
     <el-table :data="tableData">
-      <el-table-column prop="date" label="日期" width="140">
-      </el-table-column>
-      <el-table-column prop="name" label="姓名" width="120">
-      </el-table-column>
-      <el-table-column prop="address" label="地址">
-      </el-table-column>
-    </el-table>
+    <el-table-column prop="date" label="日期" width="140">
+    </el-table-column>
+    <el-table-column prop="name" label="姓名" width="120">
+    </el-table-column>
+    <el-table-column prop="address" label="地址">
+    </el-table-column>
+  </el-table>
+
   </div>
 </template>
 
