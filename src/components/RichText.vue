b756d5b614ed59b362eddeedc6f65dbece65fa86
pdeng <635639654@qq.com>
2020-03-26 00:26:44 +0800

    增加 quill 富文本编辑器组件

--- src/components/RichText.vue
+++ src/components/RichText.vue
@@ -9,9 +9,9 @@
 import Vue from 'vue'
 import VueQuillEditor from 'vue-quill-editor'
 // require styles
-import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
+// import 'quill/dist/quill.core.css'
+// import 'quill/dist/quill.bubble.css'
 Vue.use(VueQuillEditor /* { default global options } */)
 export default {
   name: 'RichText'
